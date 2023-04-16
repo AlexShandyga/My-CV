f69ff1ac9599db44a2b1b4dd68e432e98f7b0043
Oleksandr <allekssandr13@gmail.com>
2023-04-16 16:58:28 +0300

    lesson-32 complete

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const elements = [
     { key: 'b', element: document.getElementById('b') },
     { key: 'c', element: document.getElementById('c') },
     { key: 'd', element: document.getElementById('d') },
+    { key: 'e', element: document.getElementById('e') }
 ];
 
 elements.forEach(({ key, element }) => {
@@ -19,3 +20,57 @@ elements.forEach(({ key, element }) => {
         }
     });
 });
+
+fetch('https://api.github.com/users/AlexShandyga/repos')
+    .then(response => response.json())
+    .then(json => {
+        const projectList = document.querySelector('.project-list');
+        json.forEach((item, index) => {
+
+            console.log(item);
+            const projects = document.createElement('li');
+
+
+
+        })
+    })
+
+
+
+///////////////////////
+
+const url = 'https://api.github.com/users/AlexShandyga/repos'
+
+const repos = document.querySelector('.repos')
+
+function createElement(data) {
+    const repo = document.createElement('div');
+    repo.classList.add('repo');
+
+    const a = document.createElement('a');
+    a.textContent = data.full_name;
+    a.setAttribute('href', data.html_url);
+    a.setAttribute('target', '_blank');
+
+    repo.appendChild(a);
+
+    if (data.description) {
+
+        const desc = document.createElement('div');
+        desc.textContent = data.description;
+        repo.appendChild(desc);
+    }
+
+    repos.appendChild(repo);
+
+}
+
+function handleData(data) {
+    data.forEach(d => {
+        createElement(d)
+    })
+}
+
+fetch(url)
+    .then(response => response.json())
+    .then(handleData)
